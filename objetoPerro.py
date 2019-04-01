347cc5894351d21966a883d252c02a667c920bcd
meyi <rubencastilloescamilla89@gmail.com>
2019-04-01 18:48:29 +0200

    Creación de primer objeto

---
+++ objetoPerro.py
@@ -0,0 +1,15 @@
+'''
+Created on 1 abr. 2019
+
+@author: meyi
+'''
+
+class Perro():
+    
+    def __init__(self, nombre, edad, peso):
+        '''
+        Constructor
+        '''
+        self.nombre = nombre
+        self.edad = edad        
+        self.peso = peso
